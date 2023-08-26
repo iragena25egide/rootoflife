cd2e91943beab99060ad3be7cd22c3063399666f
iragena <iragenaegide205@gmail.com>
2023-08-26 17:10:44 +0200

    my message

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('ine kid')
+let container=document.querySelector('.product');
+let btn=document.getElementById('product');
+
+
+
+btn.onclick=()=>{
+    container.style.display='inherit';
+}
